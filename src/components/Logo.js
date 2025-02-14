6725790d427ef7d51b5f3144dd0454128405c8e7
unknown <dhruvarne22@gmail.com>
2025-02-14 09:34:41 +0530

    Logo Modified-CodeWithDhruv

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -10,25 +10,12 @@ const Logo = () => {
   return (
     <div 
       className='relative flex items-center justify-center mt-2' 
-      onMouseEnter={() => setIsHovered(true)} 
-      onMouseLeave={() => setIsHovered(false)}
+  
     >
-      {/* Tooltip */}
-      {isHovered && (
-        <motion.span
-          className="absolute -top-10 left-1/2 -translate-x-1/2 bg-gray-800 text-white text-sm px-3 py-1 rounded-md shadow-lg dark:bg-white dark:text-black"
-          initial={{ opacity: 0, y: 5 }}
-          animate={{ opacity: 1, y: 100 }}
-          exit={{ opacity: 0, y: -5 }}
-          transition={{ duration: 0.3 }}
-        >
-          Developed By CodeWithDhruv
-        </motion.span>
-      )}
 
       {/* Logo */}
       <MotionLink 
-        href="https://dhananjayarne.vercel.app/"
+        href="/"
         target='_blank'
         className='w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full
         text-2xl font-bold border border-solid border-transparent dark:border-light'
